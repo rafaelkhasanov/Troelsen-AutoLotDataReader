045ecae9130a623a8bf97055f0fe8fd41b0418d8
Rafael <rap1raf@gmail.com>
2019-03-24 20:33:45 +0300

    Copy from another project

---
+++ AutoLotDataReader/AutoLotDataReader/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace AutoLotDataReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"Fun With Data Readers");
+            //Предположим, что значение connectionString на самом деле получено из файла *.config
+            string connectionString = @"Data Source = (localDb)\mssqllocaldb; Initial Catalog = AutoLot; Connect Timeout = 30; Integrated Security = true";
+            //Создать строку с помощью объекта построителя
+            var cnStringBuilder = new SqlConnectionStringBuilder(connectionString)
+            {
+                //InitialCatalog = "AutoLot",
+                //DataSource = @"(localDb)\mssqllocaldb",
+                //ConnectTimeout = 30,
+                //IntegratedSecurity = true
+            };
+            //Создать и открыть подключение
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = cnStringBuilder.ConnectionString;
+                connection.Open();
+                ShowConnectionProperty(connection);
+                //Создать объект команды SQL
+                string sqlCommand = "Select * From Inventory";
+                SqlCommand myCommand = new SqlCommand(sqlCommand, connection);
+                //Получить объект чтения данных с помощью ExecuteReader
+                using (SqlDataReader dbReader = myCommand.ExecuteReader())
+                {
+                    while (dbReader.Read())
+                    {
+                        WriteLine($"-> Make: {dbReader["Make"]}, PetName: {dbReader["PetName"]}, Color: {dbReader["Color"]}");
+                    }
+                }
+            }
+            ReadLine();
+        }
+
+        static void ShowConnectionProperty(SqlConnection connection)
+        {
+            WriteLine("Info About connection");
+            WriteLine($"Database location: {connection.DataSource}");
+            WriteLine($"Database name: {connection.Database}");
+            WriteLine($"Timeout: {connection.ConnectionTimeout}");
+            WriteLine($"Connection state: {connection.State}");
+        }
+    }
+}
+
